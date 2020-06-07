0799b5b6266492689383c48e2a6ad6d823377860
Christophe <christophe@MacBook-Pro-de-MBP.local>
2020-06-07 11:02:30 +0300

    Search page

--- src/Home/SearchResult.js
+++ src/Home/SearchResult.js
@@ -2,14 +2,24 @@ import React from 'react';
 import CTitle from '../Components/CTitle';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
+import Http from '../Utils/Http';
+
+let http = new Http();
 
 export default function SearchResult() {
     const [t,] = useTranslation();
     const [search, setSearch] = React.useState("");
     const params = useParams();
+    const [rows, setRows] = React.useState([]);
     React.useEffect(() => {
-        setSearch(localStorage.getItem("search-pattern"))
+        let pattern = localStorage.getItem("search-pattern");
+        setSearch(pattern);
+        http.get(`search/${btoa(pattern)}`).then(response => {
+
+        }).catch(console.error);
     }, [params])
+
+
     return (
         <>
             <CTitle subtitle={`${t("search.subtitle")} "${search}"`}>{t("search.title")}</CTitle>
